69002e8d376839bc065d622811138eade2176aaf
emil5654 <78964396+emil5654@users.noreply.github.com>
2022-05-12 15:37:09 +0200

    Add files via upload

--- example.local/a.php
+++ example.local/a.php
@@ -1,46 +1,36 @@
 <?php
 {
-// Function to print out objects / arrays
 function PrintObj ($o) { echo "<pre>"; print_r($o); echo "</pre>"; }
 
 // Load the POST.
 $data = file_get_contents("php://input");
 
 // ...and decode it into a PHP array.
-$data = json_decode($data); 
+$jsondata = json_decode($data);
 
 // Do whatever with the array. 
-PrintObj($data);
-
-
-/*if ($_SERVER["REQUEST_METHOD"] == "GET") {
-$jsonobj = $_REQUEST['timestamp'];
-
-$arr = json_decode($jsonobj, true);
-
-echo count($arr);
-*/
-  // collect value of input field
+//PrintObj($jsondata);
 
 $servername = "localhost";
 $username = "root";
 $password = "cep2";
 $dbname = "kitchenguearddb";
-// Create connection
+
+// Database connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-$sql = "INSERT INTO event(Timestamp)
-VALUES ($data)";
-
-if (mysqli_query($conn, $sql))
-{
-  echo "New record created successfully";
+// Insert data Query
+$sql = "INSERT INTO events ( Timestamp, Eventtype, IdChain) 
+VALUES ('$jsondata->Timestamp', '$jsondata->Event' , '$jsondata->IdChain')";
+  
+if ($conn->query($sql) === TRUE) {
+  echo "Insert your JSON record successfully";
 }
 else
 {
   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
-mysqli_close($conn);
-  }
+mysqli_close($conn); 
+}
 ?>
+
